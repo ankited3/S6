ebf2191ae7f3dd9da86cb89f6937e8db9583fe4b
Ankit Jain <ankit911991.gmail.com>
2025-09-27 00:08:15 +0530

    updated batch size

--- model_3.py
+++ model_3.py
@@ -47,7 +47,7 @@ if cuda:
     torch.cuda.manual_seed(SEED)
 
 # dataloader arguments - something you'll fetch these from cmdprmt
-dataloader_args = dict(shuffle=True, batch_size=128, num_workers=4, pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
+dataloader_args = dict(shuffle=True, batch_size=64, num_workers=4, pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
 
 # train dataloader
 train_loader = torch.utils.data.DataLoader(train, **dataloader_args)
